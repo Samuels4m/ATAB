113867986e9931cc9c7f850b6d443b6a87e95d2e
samuel <samuel@samuel-notebook>
2012-10-19 17:26:31 -0300

    Refeita a chave

--- AgendaTelefonica/src/arvore/Chave.java
+++ AgendaTelefonica/src/arvore/Chave.java
@@ -9,5 +9,28 @@ package arvore;
  * @author samuel
  */
 public class Chave {
+    private char[] nome = new char[30];
+    private int inicio;
+
+    public Chave(char[] nome, int inicio) {
+        this.nome = nome;
+        this.inicio = inicio;
+    }
+
+    public char[] getNome() {
+        return nome;
+    }
+
+    public void setNome(char[] nome) {
+        this.nome = nome;
+    }
+
+    public int getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(int inicio) {
+        this.inicio = inicio;
+    }
     
 }
