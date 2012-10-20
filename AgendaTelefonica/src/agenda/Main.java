6aea10e8d672ca62a964f4dc0db70b46a5273e85
samuel <samuel@samuel-notebook>
2012-10-19 23:43:34 -0300

    Fiz testes com o arquivo, utilizando random acess, fico daora

--- AgendaTelefonica/src/agenda/Main.java
+++ AgendaTelefonica/src/agenda/Main.java
@@ -4,17 +4,100 @@
  */
 package agenda;
 
+import java.beans.beancontext.BeanContext;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import sun.io.CharToByteASCII;
+
 /**
  *
  * @author samuel
  */
 public class Main {
+
     public static void main(String args[]) {
-        char[] nome = new char[30];
-        char[] end = new char[50];
-        char[] tel = new char[15];
+        try {
+            try {
+                String nome = "Samuel";
+                String end = "Rua";
+                String tel = "3030";
+                byte[] n = new byte[30];
+                byte[] e = new byte[50];
+                byte[] t = new byte[15];
+
+                for (int i = 0; i < nome.length(); i++) {
+                    n[i] = (byte) nome.charAt(i);
+                }
+                for (int i = 0; i < end.length(); i++) {
+                    e[i] = (byte) end.charAt(i);
+                }
+                 for (int i = 0; i < tel.length(); i++) {
+                    t[i] = (byte) tel.charAt(i);
+                }
+     
+
+                /*
+                 * 
+                 */
+
+       
+
+                /*
+                 * 
+                 */
+
+               
+                    File file = new File("dados.txt");
+                RandomAccessFile raf;
+                    
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    
+                    int fim = 0;
+                    
+                    raf = new RandomAccessFile(file, "rw");
+                    
+                    raf.seek(fim);
+                    raf.write(n);
+                    raf.write(e);
+                    raf.write(t);
+                    raf.close();
+                
+
+            
+     
+
+                /*
+                 * 
+                 */
+
+
+              
+            } catch (IOException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+             File file = new File("dados.txt");
+                RandomAccessFile raf;
+                    
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    
+                    int fim = 0;
+                    
+                    raf = new RandomAccessFile(file, "rw");
+                    byte[] n = new byte[30];
+                    raf.seek(fim);
+                    raf.read(n);
+                    String teste = new String(n, "UTF-8");
+                    System.out.println(teste);
+                    raf.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
-        
-        Pessoa pessoa = new Pessoa('a', 'b', 'c');
     }
 }
